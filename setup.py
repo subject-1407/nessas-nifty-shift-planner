d0a520b0f12638b05ffd5e6c0659a81b65b10b1f
subject <subject@threepixels.de>
2026-01-06 11:34:02 +0100

    Cleanup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name='nessas-nifty-shift-planner',
+	version='1.0',
+	packages=['nessas-nifty-shift-planner'],
+	url='https://github.com/subject-1407/nessas-nifty-shift-planner',
+	license='MIT License',
+	author='subject',
+	author_email='subject@threepixels.de',
+	description=' As simple shift planner. '
+)
